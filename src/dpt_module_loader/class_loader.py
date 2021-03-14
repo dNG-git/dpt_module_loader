1a0cc97e0ddbc2436129e11bc99eb2afcab3c1c4
NotTheEvilOne <git-NTEO@vplace.de>
2021-03-14 20:34:05 +0100

    Replace "__slots__" lists with tuples
    (Trivial)

--- src/dpt_module_loader/class_loader.py
+++ src/dpt_module_loader/class_loader.py
@@ -35,7 +35,7 @@ and class name given as "package.module:Class".
              Mozilla Public License, v. 2.0
     """
 
-    __slots__ = [ "__weakref__", "config" ]
+    __slots__ = ( "__weakref__", "config" )
     """
 python.org: __slots__ reserves space for the declared variables and prevents
 the automatic creation of __dict__ and __weakref__ for each instance.
